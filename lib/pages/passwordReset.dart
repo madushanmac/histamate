aaab93b9d7df4cd5b2f4b62a411112e229e168b6
madushanmac <chamindumadushan1996org@gmail.com>
2024-07-21 16:54:01 +0530

    fixed bugs users login , completed ingredient filter section

--- lib/pages/passwordReset.dart
+++ lib/pages/passwordReset.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hista_mate/pages/login_screen.dart';
-import 'package:hista_mate/pages/register_screen.dart';
 import '../Components/InputTextField.dart';
 import '../Components/Primary_button.dart';
 import '../Components/SqureTile.dart';
