2dd7fd34e90c3168aed90b3d26f977c4713f55db
madushanmac <chamindumadushan1996org@gmail.com>
2024-07-28 00:46:30 +0530

    done the changes 07/28

--- lib/pages/calender_screen.dart
+++ lib/pages/calender_screen.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class CalendarScreen extends StatefulWidget {
